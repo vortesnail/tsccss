be9733bccbb56b774ec68d724130dac56f5cc2f6
vortesnail <1091331061@qq.com>
2021-03-17 20:25:46 +0800

    chore: code format settings

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'lines-between-class-members': [ERROR, 'always'],
+    'linebreak-style': [ERROR, 'unix'],
+    quotes: [ERROR, 'single'],
+    semi: [ERROR, 'always'],
+    'no-unused-expressions': WARN,
+    'no-plusplus': OFF,
+    'no-console': OFF,
+    'class-methods-use-this': ERROR,
+    'global-require': OFF,
+    'no-use-before-define': OFF,
+    'no-restricted-syntax': OFF,
+    'no-continue': OFF,
+  },
+};
